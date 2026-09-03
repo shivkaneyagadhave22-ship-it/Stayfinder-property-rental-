3b7bde9e33f7c747eb74d7bb384ded906d85529a
Shivkanya Gadhave <shivkaneyagadhve22@gmail.com>
2026-09-04 02:01:07 +0530

    LLC

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
@@ -35,8 +35,7 @@ const io = new Server(server, {
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true,
+    origin: "*",
   })
 );
 
